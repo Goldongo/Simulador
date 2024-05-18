5f3bb1f2f675f46014f4cb7a411676ce55faf1f4
Snah-s <camiloernestosoto@gmail.com>
2024-05-17 20:22:49 -0500

    ✨ Update Simulator

--- api/sim.py
+++ api/sim.py
@@ -1,42 +1,42 @@
 import random
 
 class Player:
-    def __init__(self, name, overall, team, position):
-        self.name = name
-        self.overall = overall
-        self.team = team
-        self.position = position
+  def __init__(self, name, overall, team, position):
+    self.name = name
+    self.overall = overall
+    self.team = team
+    self.position = position
 
-    def __str__(self):
-        return f"Name: {self.name}, Overall: {self.overall}"
+  def __str__(self):
+    return f"Name: {self.name}, Overall: {self.overall}"
 
 class Team:
-    def __init__(self, name, color, players):
-        self.name = name
-        self.color = color
-        self.players = players
-        self.lines = {
-            "GK": [p for p in players if p.position == "GK"],
-            "DF": [p for p in players if p.position in ["CB", "FB"]],
-            "CDM": [p for p in players if p.position == "CDM"],
-            "CM": [p for p in players if p.position in ["CM", "WM"]],
-            "CAM": [p for p in players if p.position in ["CAM", "WAM"]],
-            "AT": [p for p in players if p.position in ["ST", "CF", "FW"]]
-        }
-        self.defending = self.calculate_average(["GK", "DF", "CDM"])
-        self.control = self.calculate_average(["CAM", "CM", "CDM"])
-        self.attacking = self.calculate_average(["CM", "CAM", "AT"])
+  def __init__(self, name, color, players):
+    self.name = name
+    self.color = color
+    self.players = players
+    self.lines = {
+      "GK": [p for p in players if p.position == "GK"],
+      "DF": [p for p in players if p.position in ["CB", "FB"]],
+      "CDM": [p for p in players if p.position == "CDM"],
+      "CM": [p for p in players if p.position in ["CM", "WM"]],
+      "CAM": [p for p in players if p.position in ["CAM", "WAM"]],
+      "AT": [p for p in players if p.position in ["ST", "CF", "FW"]]
+    }
+    self.defending = self.calculate_average(["GK", "DF", "CDM"])
+    self.control = self.calculate_average(["CAM", "CM", "CDM"])
+    self.attacking = self.calculate_average(["CM", "CAM", "AT"])
 
-    def calculate_average(self, positions):
-        total_overall = sum([player.overall for pos in positions for player in self.lines[pos]])
-        total_players = sum([len(self.lines[pos]) for pos in positions])
-        return round(total_overall / total_players) if total_players else 0
+  def calculate_average(self, positions):
+    total_overall = sum([player.overall for pos in positions for player in self.lines[pos]])
+    total_players = sum([len(self.lines[pos]) for pos in positions])
+    return round(total_overall / total_players) if total_players else 0
 
-    def __str__(self):
-        return f"Name: {self.name}, Players: [{', '.join([str(p) for p in self.players])}]"
+  def __str__(self):
+    return f"Name: {self.name}, Players: [{', '.join([str(p) for p in self.players])}]"
 
 def no_repeat(array1, array2):
-    return not set(array1).intersection(set(array2))
+  return not set(array1).intersection(set(array2))
 
 GOALKEEPER_WEIGHT = 0.1
 DEFENSELINE_WEIGHT = 1.5
@@ -46,65 +46,66 @@ FRONTMIDFIELDLINE_WEIGHT = 4
 ATTACKLINE_WEIGHT = 6
 
 WEIGHTS = {
-    "GK": GOALKEEPER_WEIGHT,
-    "DF": DEFENSELINE_WEIGHT,
-    "CDM": BACKMIDFIELDLINE_WEIGHT,
-    "CM": MIDFIELDLINE_WEIGHT,
-    "CAM": FRONTMIDFIELDLINE_WEIGHT,
-    "AT": ATTACKLINE_WEIGHT
+  "GK": GOALKEEPER_WEIGHT,
+  "DF": DEFENSELINE_WEIGHT,
+  "CDM": BACKMIDFIELDLINE_WEIGHT,
+  "CM": MIDFIELDLINE_WEIGHT,
+  "CAM": FRONTMIDFIELDLINE_WEIGHT,
+  "AT": ATTACKLINE_WEIGHT
 }
 
 def check_lines(team):
-    lines = [line for line in team.lines if team.lines[line]]
-    weights = [WEIGHTS[line] for line in lines]
-    return lines, weights
+  lines = [line for line in team.lines if team.lines[line]]
+  weights = [WEIGHTS[line] for line in lines]
+  return lines, weights
 
 def game(team1, team2):
-    BASE_ATTACKS_NUM = 8
-    regularTime = 90
-    
-    score = [0, 0]
-    
+  BASE_ATTACKS_NUM = 8
+  regularTime = 90
+  
+  score = [0, 0]
+  
+  team1Opportunities = random.sample(range(1, 100), team1.control // 10)
+  team2Opportunities = random.sample(range(1, 100), team2.control // 10)
+  
+  while no_repeat(team1Opportunities, team2Opportunities):
     team1Opportunities = random.sample(range(1, 100), team1.control // 10)
     team2Opportunities = random.sample(range(1, 100), team2.control // 10)
-    
-    while no_repeat(team1Opportunities, team2Opportunities):
-        team1Opportunities = random.sample(range(1, 100), team1.control // 10)
-        team2Opportunities = random.sample(range(1, 100), team2.control // 10)
-    
+  
+  team1Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team1.attacking - team2.defending) // 10)
+  team2Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team2.attacking - team1.defending) // 10)
+  
+  while no_repeat(team1Attacks, team2Attacks):
     team1Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team1.attacking - team2.defending) // 10)
     team2Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team2.attacking - team1.defending) // 10)
-    
-    while no_repeat(team1Attacks, team2Attacks):
-        team1Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team1.attacking - team2.defending) // 10)
-        team2Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team2.attacking - team1.defending) // 10)
-    
-    time = 0
-    
-    team1Lines, team1Weights = check_lines(team1)
-    team2Lines, team2Weights = check_lines(team2)
-    
-    while time < regularTime:
-        randNum = random.randint(1, 100)
-        if randNum in team1Opportunities:
-            randNum = random.randint(1, 100)
-            if randNum in team1Attacks:
-                score[0] += 1
-                print(f"GOOOOOL del {team1.name}!!!")
-                goalscorerLine = random.choices(team1Lines, team1Weights)[0]
-                goalscorer = random.choice(team1.lines[goalscorerLine])
-                print(f"  {goalscorer.name}, {time}")
+  
+  time = 0
+  
+  team1Lines, team1Weights = check_lines(team1)
+  team2Lines, team2Weights = check_lines(team2)
+  
+  while time < regularTime:
+    randNum = random.randint(1, 100)
+    if randNum in team1Opportunities:
+      randNum = random.randint(1, 100)
+      if randNum in team1Attacks:
+        score[0] += 1
+        print(f"GOOOOOL del {team1.name}!!!")
+        goalscorerLine = random.choices(team1Lines, team1Weights)[0]
+        goalscorer = random.choice(team1.lines[goalscorerLine])
+        print(f"  {goalscorer.name}, {time}")
 
-        if randNum in team2Opportunities:
-            randNum = random.randint(1, 100)
-            if randNum in team2Attacks:
-                score[1] += 1
-                print(f"GOOOOOL del {team2.name}!!!")
-                goalscorerLine = random.choices(team2Lines, team2Weights)[0]
-                goalscorer = random.choice(team2.lines[goalscorerLine])
-                print(f"  {goalscorer.name}, {time}")
-                
-        time += 1
+    if randNum in team2Opportunities:
+      randNum = random.randint(1, 100)
+      if randNum in team2Attacks:
+        score[1] += 1
+        print(f"GOOOOOL del {team2.name}!!!")
+        goalscorerLine = random.choices(team2Lines, team2Weights)[0]
+        goalscorer = random.choice(team2.lines[goalscorerLine])
+        print(f"  {goalscorer.name}, {time}")
 
-    print()
-    print(f"Final: {team1.name} {score[0]} - {score[1]} {team2.name}")
+    time += 1
+
+  print()
+  print(f"Final: {team1.name} {score[0]} - {score[1]} {team2.name}")
+  
--- api/sim_api.py
+++ api/sim_api.py
@@ -8,36 +8,46 @@ app = FastAPI()
 
 class Player(BaseModel):
   name: str
-  rating: int
+  overall: int
   team: str
   position: str
+  
+  def __init__(self, **data):
+    super().__init__(**data)
+  
+  def __str__(self):
+    return f"Name: {self.name}, Overall: {self.rating}"
 
 class Team(BaseModel):
   name: str
   color: str
   players: List[Player]
-  goalkeeper: List[Player] = []
-  defenseLine: List[Player] = []
-  backMidfieldLine: List[Player] = []
-  midfieldLine: List[Player] = []
-  frontMidfieldLine: List[Player] = []
-  attackLine: List[Player] = []
-  defending: int = 0
-  control: int = 0
-  attacking: int = 0
-
+  lines: dict
+  defending: int
+  control: int
+  attacking: int
+  
   def __init__(self, **data):
     super().__init__(**data)
-    self.goalkeeper = [i for i in self.players if i.position == "GK"]
-    self.defenseLine = [i for i in self.players if i.position in ["CB", "FB"]]
-    self.backMidfieldLine = [i for i in self.players if i.position == "CDM"]
-    self.midfieldLine = [i for i in self.players if i.position in ["CM", "WM"]]
-    self.frontMidfieldLine = [i for i in self.players if i.position in ["CAM", "WAM"]]
-    self.attackLine = [i for i in self.players if i.position in ["ST", "CF", "FW"]]
+    self.lines = {
+      "GK": [p for p in self.players if p.position == "GK"],
+      "DF": [p for p in self.players if p.position in ["CB", "FB"]],
+      "CDM": [p for p in self.players if p.position == "CDM"],
+      "CM": [p for p in self.players if p.position in ["CM", "WM"]],
+      "CAM": [p for p in self.players if p.position in ["CAM", "WAM"]],
+      "AT": [p for p in self.players if p.position in ["ST", "CF", "FW"]]
+    }
+    self.defending = self.calculate_average(["GK", "DF", "CDM"])
+    self.control = self.calculate_average(["CAM", "CM", "CDM"])
+    self.attacking = self.calculate_average(["CM", "CAM", "AT"])
+  
+  def calculate_average(self, positions):
+    total_overall = sum([player.overall for pos in positions for player in self.lines[pos]])
+    total_players = sum([len(self.lines[pos]) for pos in positions])
+    return round(total_overall / total_players) if total_players else 0
 
-    self.defending = round((sum([i.rating for i in self.goalkeeper]) + sum([i.rating for i in self.defenseLine]) + sum([i.rating for i in self.backMidfieldLine])) / (len(self.defenseLine) + len(self.backMidfieldLine) + len(self.goalkeeper)))
-    self.control = round((sum([i.rating for i in self.frontMidfieldLine if i.position == "CAM"]) + sum([i.rating for i in self.midfieldLine]) + sum([i.rating for i in self.backMidfieldLine])) / (len([i.rating for i in self.frontMidfieldLine if i.position == "CAM"]) + len(self.midfieldLine) + len(self.backMidfieldLine)))
-    self.attacking = round((sum([i.rating for i in self.midfieldLine if i.position == "WM"]) + sum([i.rating for i in self.frontMidfieldLine]) + sum([i.rating for i in self.attackLine])) / (len([i.rating for i in self.midfieldLine if i.position == "WM"]) + len(self.frontMidfieldLine) + len(self.attackLine)))
+  def __str__(self):
+    return f"Name: {self.name}, Players: [{', '.join([str(p) for p in self.players])}]"
 
 @app.get("/teams/{id}")
 async def get_team(id: int):
@@ -53,11 +63,35 @@ async def get_team(id: int):
     raise HTTPException(status_code=response.status_code, detail="Team not found")
 
 def noRepeat(array1, array2):
-  set1 = set(array1)
-  set2 = set(array2)
-  return not set1.intersection(set2)
+  return not set(array1).intersection(set(array2))
 
-def game(team1: Team, team2: Team):
+GOALKEEPER_WEIGHT = 0.1
+DEFENSELINE_WEIGHT = 1.5
+BACKMIDFIELDLINE_WEIGHT = 2
+MIDFIELDLINE_WEIGHT = 3
+FRONTMIDFIELDLINE_WEIGHT = 4
+ATTACKLINE_WEIGHT = 6
+
+WEIGHTS = {
+  "GK": GOALKEEPER_WEIGHT,
+  "DF": DEFENSELINE_WEIGHT,
+  "CDM": BACKMIDFIELDLINE_WEIGHT,
+  "CM": MIDFIELDLINE_WEIGHT,
+  "CAM": FRONTMIDFIELDLINE_WEIGHT,
+  "AT": ATTACKLINE_WEIGHT
+}
+
+def check_lines(team):
+  lines = [line for line in team.lines if team.lines[line]]
+  weights = [WEIGHTS[line] for line in lines]
+  return lines, weights
+
+def check_lines(team):
+  lines = [line for line in team.lines if team.lines[line]]
+  weights = [WEIGHTS[line] for line in lines]
+  return lines, weights
+
+def game(team1, team2):
   BASE_ATTACKS_NUM = 8
   regularTime = 90
   score = [0, 0]
@@ -72,23 +106,36 @@ def game(team1: Team, team2: Team):
     team1Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team1.attacking - team2.defending) // 10)
     team2Attacks = random.sample(range(1, 100), BASE_ATTACKS_NUM + (team2.attacking - team1.defending) // 10)
   time = 0
+  
+  team1Lines, team1Weights = check_lines(team1)
+  team2Lines, team2Weights = check_lines(team2)
+  
+  output = []
+  
   while time < regularTime:
     randNum = random.randint(1, 100)
     if randNum in team1Opportunities:
       randNum = random.randint(1, 100)
       if randNum in team1Attacks:
         score[0] += 1
+        goalscorerLine = random.choices(team1Lines, team1Weights)[0]
+        goalscorer = random.choice(team1.lines[goalscorerLine])
+        output.append(f"{team1.name} GOOOOOL!!! {goalscorer.name} {time}")
     if randNum in team2Opportunities:
       randNum = random.randint(1, 100)
       if randNum in team2Attacks:
         score[1] += 1
+        goalscorerLine = random.choices(team2Lines, team2Weights)[0]
+        goalscorer = random.choice(team2.lines[goalscorerLine])
+        output.append(f"{team2.name} GOOOOOL!!! {goalscorer.name} {time}")
     time += 1
-  return score
+  
+  return score, output
 
 @app.post("/game/{team1_id}/{team2_id}")
 async def simulate_game(team1_id: int, team2_id: int):
   team1 = await get_team(team1_id)
   team2 = await get_team(team2_id)
   
-  score = game(team1, team2)
-  return {"result": f"{team1.name} {score[0]} - {score[1]} {team2.name}"}
+  score, output = game(team1, team2)
+  return score, output
